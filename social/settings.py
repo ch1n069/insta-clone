bd5e22ac398d0c3e6d5c27c4141867bcaeede150
bruno <brunolalpachino@gmail.com>
2022-06-05 23:11:40 +0300

    tweaked with the settings

--- social/settings.py
+++ social/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'insta'
+    # 'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -121,9 +122,13 @@ STATIC_URL = 'static/'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# CRISPY_TEMPLATE_PACK = 'bootstrap5'
