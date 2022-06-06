a55964907b6a49f68b9bf39b7c62213d4b14605d
bruno <brunolalpachino@gmail.com>
2022-06-06 18:15:55 +0300

    added the form to add new post and method in models to redirect

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -15,3 +15,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('post-detail', args=(str(self.id)))
