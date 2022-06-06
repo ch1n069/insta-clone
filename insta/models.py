e2bd90b14fc70021749fc85c8ff9a4eba693590b
bruno <brunolalpachino@gmail.com>
2022-06-06 22:45:09 +0300

    added the pubdate in our model

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 # Create your models here.
 
 
@@ -9,6 +10,7 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=100,)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
 
 
 
