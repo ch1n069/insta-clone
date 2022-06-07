21a407edcf88c6eb7dc20d6a12f516d59c031bd3
bruno <brunolalpachino@gmail.com>
2022-06-07 19:26:12 +0300

    changed the forms.py

--- insta/forms.py
+++ insta/forms.py
@@ -1,16 +1,23 @@
 from django import forms  
-from insta.models import Post
+from insta.models import Post , HashTag 
 
 
+choices = HashTag.objects.all().values_list('name','name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author','body')
+        fields = ('title', 'title_tag', 'author','hash_tag','body')
         widgets = {
             'title':forms.TextInput(attrs={'class':'form-control'}),
             'title_tag':forms.TextInput(attrs={'class':'form-control'}),
             'author':forms.Select(attrs={'class':'form-control'}),
+            'hash_tag':forms.Select(choices=choice_list,attrs={'class':'form-control'}),
             'body':forms.Textarea(attrs={'class':'form-control'}),
        
         }
