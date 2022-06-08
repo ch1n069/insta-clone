d2df2ac35a77892225b9d3a6ee30e4203eaed83c
bruno <brunolalpachino@gmail.com>
2022-06-08 13:18:12 +0300

    modified the urls

--- insta/urls.py
+++ insta/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 
-from insta.views import HomeView, ArticleDetailView , AddPostView , UpdatePostView , DeletePostView , UserPostView ,LikeView
+from insta.views import HomeView, ArticleDetailView , AddPostView , UpdatePostView , DeletePostView , UserPostView 
 
 
 
@@ -12,7 +12,6 @@ urlpatterns = [
     path('add_post/', AddPostView.as_view(), name="add_post"),
     path('update_post/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
     path('delete_post/edit/<int:pk>/delete', DeletePostView.as_view(), name="delete_post"),
-    path('like/<int:pk>', LikeView, name='like_post'),
     path('user_posts/', UserPostView.as_view(), name="user_posts")
 
 
