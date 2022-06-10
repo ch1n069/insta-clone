ab12bba84d22454bb87a4a424d4b8cbcd6596461
bruno <brunolalpachino@gmail.com>
2022-06-10 12:20:06 +0300

    the setting for deployment

--- social/settings.py
+++ social/settings.py
@@ -17,7 +17,6 @@ from decouple import config,Csv
 
 
 
-
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -25,12 +24,12 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 if config('MODE')=="dev":
    DATABASES = {
        'default': {
-        'ENGINE': 'django.db.backends.postgresql',     
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
        }
        
    }
