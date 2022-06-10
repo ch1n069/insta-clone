921223832289ea2a10483b32a52bfedddca046ce
bruno <brunolalpachino@gmail.com>
2022-06-10 11:50:18 +0300

    allowed hosts

--- social/settings.py
+++ social/settings.py
@@ -69,15 +69,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'insta',
+    'members',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'insta',
-    'members',
-   
 ]
 
 MIDDLEWARE = [
